19d506afa8c5fe385f9b9022d49bf562a45d0314
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2025-12-13 14:08:59 +0100

    Created the navigation section of the header

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,8 +1,21 @@
-
+import './PageHeader.css'
 export  const PageNavigation = ()=>{
     return(
-        <nav>
-            This is the navigation
+        <nav className="nav">
+            <div className="nav__logo">
+                <img src="/logo.png" alt="" />
+            </div>
+            <ul className="nav__menu">
+                <li><a href=""> Home</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Courses</a></li>
+                <li><a href="">Pages</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div className="nav__action">
+                <button>Create Account</button>
+            </div>
         </nav>
     )
 }
